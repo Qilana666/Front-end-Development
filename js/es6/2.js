1cacc32ca41ec2a392aaea74565bed7b1e23be09
绮岚 <3368750986@qq.com>
2025-11-16 23:49:48 +0800

    feat(es6): 添加ES6+特性示例代码和文档
    添加多个ES6+特性示例文件，包括解构赋值、rest参数、模板字符串、for-of循环、BigInt等
    添加readme.md文档说明ES6+主要特性

---
+++ js/es6/2.js
@@ -0,0 +1,9 @@
+let myname = 'bailandi';
+console.log('Hello, I am ' + myname);
+// ${myName} 插值表达式
+console.log(`Hello, I am ${myname}`);
+console.log(`Hello, I am ${myname.toUpperCase()}`);
+
+for (let x of myname) {
+    console.log(x);
+}
