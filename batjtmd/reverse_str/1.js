b6502b183342f630ec7ad681eb7ebeeed4daebdd
绮岚 <3368750986@qq.com>
2025-11-22 22:06:23 +0800

    docs(reverse_str): 增加字符串反转函数的注释说明
    在reverse_str/1.js文件中添加了对字符串反转函数的详细注释，解释了split、reverse和join方法的作用，提升了代码的可读性和理解性。

--- batjtmd/reverse_str/1.js
+++ batjtmd/reverse_str/1.js
@@ -4,5 +4,8 @@ function reverseStr(str) {
     //数组可以反转
     //数组join 拼接字符串
     return str.split('').reverse().join('')
+    //1.split('') 切割字符串 返回数组
+    //2.reverse() 反转数组**作用于数组**
+    //3.join('') 拼接字符串 返回字符串
 }
 console.log(reverseStr('hello'));
