5b4c12702fc918ef0ae73ec3e6651eefc16a47ac
绮岚 <3368750986@qq.com>
2026-01-30 13:21:41 +0800

    feat(todo): 添加持久化中间件并定义TodoState接口
    添加zustand的persist中间件以实现状态持久化
    定义TodoState接口明确todos和相关操作方法的类型

--- project/ai_fullstack1/zustand-demo/src/store/todo.ts
+++ project/ai_fullstack1/zustand-demo/src/store/todo.ts
@@ -1,5 +1,10 @@
 import { create } from 'zustand';
-import { Todo } from '../types/index';
+import type { Todo } from '../types/index';
+import { persist } from 'zustand/middleware'; 
 
-
-
+export interface TodoState {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
